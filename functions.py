bfdc910127c23c4c59ae91204dfed9877386d88b
Nikolai Gusarov <Nikolai_polos@mail.ru>
2024-01-20 11:33:37 +0300

    added function to remove duplicates

--- functions.py
+++ functions.py
@@ -1,22 +1,14 @@
-import pandas as pd
+
 import csv
 import re
 
+
 file_csv = 'phonebook_raw.csv'
-
-# Для наглядности
-def visualization(file):
-    phonebook_raw_pd = pd.read_csv(file, sep=',', encoding='utf-8')
-    return print(phonebook_raw_pd)
-    # display(phonebook_raw_pd) # в pc не работает
-    # return phonebook_raw_pd.head(3)
-
 
 def anti_chaos(file):
     with open(file, encoding="utf-8") as f:
         rows = csv.reader(f, delimiter=",")
         contacts_list = list(rows)  #
-    # pp(contacts_list)
     nested_list = []
 
     # Создаем список удобный для дальнейшей работы.
@@ -31,12 +23,14 @@ def anti_chaos(file):
         nested_list.append(contact_info)
 
     # ИСПРАВЛЯЮ БОРДАК В ФИО
-    # Берем отдельно каждый контакт...
+    # Берем отдельно контакт
     for contact in nested_list:
         full_name = contact[0][0]
-        names = full_name.split()
+        names = full_name.split()  # колонка фамилия
+        full_firstname = contact[1][0]
+        firstnames = full_firstname.split()  # колонка имя
 
-        # Обновляем информацию о контакте
+        # Обновляем информацию о контакте в колонке фамилия
         if len(names) == 3:  # Фамилия, имя и отчество присутствуют.
             contact[0] = [names[0]]  # lastname
             contact[1] = [names[1]]  # firstname
@@ -45,10 +39,15 @@ def anti_chaos(file):
             contact[0] = [names[0]]  # lastname
             contact[1] = [names[1]]  # firstname
             contact[2] = ['']  # surname отсутствует
-        elif len(names) == 1:  # есть только фамилия
+        elif len(names) == 1:  # еасть только фамилия
             contact[0] = [names[0]]  # lastname
             contact[1] = [''] if not contact[1] else contact[1]
             contact[2] = [''] if not contact[2] else contact[2]
+
+        # Обновляем информацию о контакте в колонке имя
+        if len(firstnames) == 2:  # имя и отчество присутствуют.
+            contact[1] = [firstnames[0]]  # firstname
+            contact[2] = [firstnames[1]]  # surname
 
     # исправляем телефонные номера
     for contact in nested_list:
@@ -59,14 +58,37 @@ def anti_chaos(file):
                 str(contact[5])
             )
         except TypeError:
-            # На случай contact[5] не является строковым или байтовым объектом.
+            # Обрабатывать случаи, когда contact[5] не является строковым или байтовым объектом.
             contact[5] = ''
 
-    # Приводим список к первоначальному виду для записи в csv файл.
-    flattened_list = [[item[0] if isinstance(item, list) else item for item in sublist] for sublist in nested_list]
+    # УБИРАЕМ ДУБЛИ
+    dict_contacts = {}  # создаем пустой словарь
+    for contact in nested_list[0:]:
+        key = (contact[0][0], contact[1][0])  # вкладываем в ключ фамилию и имя
+        if key not in dict_contacts:  # если имя фамилия нет в словаре
+            value = (contact[2][0], contact[3][0], contact[4][0], contact[5][2:-2], contact[6][
+                0])  # то добавляем в значение оставшиеся данные контакта заодно избавляемся от не нужных скобок
+            dict_contacts[key] = value  # и добавляем в словарь
+        elif key in dict_contacts.keys():  # если имя фамилия есть в словаре
+            value = (contact[2][0], contact[3][0], contact[4][0], contact[5][2:-2], contact[6][0])
+            new_value = []
+            for q, w in zip(dict_contacts[key], value):
+                if q == w:
+                    new_value.append(q)
+                elif q == '' and w != '':
+                    new_value.append(w)
+                elif q != '' and w == '':
+                    new_value.append(q)
+                else:
+                    new_value.append('')
+            dict_contacts[key] = tuple(new_value)
 
-    # Записываем исправленные данные в новый файл.
-    with open("phonebook.csv", "w", encoding="utf-8") as f:
+    # ОБРАТНО В СПИСОК
+    final_list = []
+    for key, value in dict_contacts.items():
+        contact_list = list(key) + list(value)
+        final_list.append(contact_list)
+
+    with open("phonebook.csv", "w", encoding="utf-8") as f:  # записываем результат в csv файл
         datawriter = csv.writer(f, delimiter=',')
-        # Вместо contacts_list подставьте свой список
-        datawriter.writerows(flattened_list)
+        datawriter.writerows(final_list)
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from functions import visualization, anti_chaos
+from functions import anti_chaos
 
 file_csv = 'phonebook_raw.csv'
-
-visualization(file_csv)
 anti_chaos(file_csv)
-
 new_file = 'phonebook.csv'
-visualization(new_file)
+
